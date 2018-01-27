afd811721d3d4c20a70de325b0c1efe238b60dbf
NadimRai <rai_nadim@yahoo.com>
2018-01-27 21:00:00 +0800

    GET route

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const todoRoutes = require('./routes/todos');
+
 app.get('/', (req,res) =>{
     res.json({message: 'Hello World'});
 })
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, () =>{
     console.log(`Running on ${port}`);
