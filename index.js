24b2f1a3ac0ca014a01738b1e872bc91610e1c17
NadimRai <rai_nadim@yahoo.com>
2018-01-27 22:47:39 +0800

    REFACTOR ROUTES

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req,res) =>{
-    res.json({message: 'Hello World'});
+    res.sendFile("index.html");
 })
 
 app.use('/api/todos', todoRoutes);
--- routes/todos.js
+++ routes/todos.js
@@ -1,55 +1,16 @@
 var express = require('express');
 var router =  express.Router();
 var db = require('../models');
+var helpers = require('../helpers/todos');
 
-router.get('/', (req,res) =>{
-   db.Todo.find()
-   .then(function(todos){
-       res.json(todos);
-   })
-   .catch(function(err){
-      res.send(err);
-   })
-});
+router.route('/')
+    .get(helpers.getTodos)
+    .post(helpers.createTodo)
 
-router.post("/", (req,res) =>{
-    db.Todo.create(req.body)
-    .then((newTodo) =>{
-        res.status(201).json(newTodo)
-    })
-    .catch((err) =>{
-        res.send(err);
-    })
-})
 
-router.get('/:todoId', function(req,res){
-   db.Todo.findById(req.params.todoId)
-   .then(function(foundTodo){
-      res.json(foundTodo)
-   })
-   .catch(function(err){
-       res.send(err);
-   })
-})
-
-router.put('/:todoId', function(req,res){
-    db.Todo.findByIdAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-    .then(function(todo){
-        res.json(todo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-})
-
-router.delete('/:todoId', function(req,res){
-    db.Todo.remove({_id: req.params.todoId})
-    .then(function(){
-        res.json({message: 'Deleted Successfully!'});
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-})
+router.route('/:todoId')
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.removeTodo)
 
 module.exports = router;
